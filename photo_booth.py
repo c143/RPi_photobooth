e6fa885d08e3a16c242e588dd150222a50712b2e
c143 <c143@users.noreply.github.com>
2015-06-07 17:38:37 +0200

    Update photo_booth.py

--- photo_booth.py
+++ photo_booth.py
@@ -6,7 +6,7 @@ while True:
 	snap = 0
 	while snap < 4:
 		gpout = subprocess.check_output("gphoto2 --capture-image-and-download --filename /home/pi/photobooth_images/photobooth%Y%m%d%H%M%S.jpg", stderr=subprocess.STDOUT, shell=True)
-	    snap += 1
+		snap += 1
 		time.sleep(0.5)
 	subprocess.call("sudo /home/pi/scripts/photobooth/assemble_and_print", shell=True)
 	time.sleep(110)
